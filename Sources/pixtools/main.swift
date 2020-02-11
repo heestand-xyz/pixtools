43346169bf256e6c40162e380fbe3c854076da7d
Hexagons <anton@hexagons.se>
2020-02-11 23:02:11 +0100

    test

--- Sources/pixtools/main.swift
+++ Sources/pixtools/main.swift
@@ -1,7 +1,25 @@
+import AppKit
 import LiveValues
-print(LiveColor(hex: "FF8000").description)
-
 import RenderKit
-//print(NODERepView(node: <#T##NODE#>))
-
 import PixelKit
+
+print("pixtools")
+
+// https://stackoverflow.com/questions/39815054/how-to-include-assets-resources-in-a-swift-package-manager-library
+// dump(Bundle.allBundles)
+
+PixelKit.main.render.engine.renderMode = .manual
+
+let ply = PolygonPIX(at: ._1024)
+
+
+//let group = DispatchGroup()
+//group.enter()
+try! PixelKit.main.render.engine.manuallyRender {
+    let img: NSImage = ply.renderedImage!
+    print("did render")
+//    group.leave()
+}
+//group.wait()
+
+print("final")
